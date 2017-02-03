2c9ed92772ae50038df4ee209f88ac51929a5d20
止水清潇 <anlin781205936@126.com>
2017-02-04 00:44:16 +0800

    Update Functions.java

--- FollowingLocalTest/src/DAGH_Following/Functions.java
+++ FollowingLocalTest/src/DAGH_Following/Functions.java
@@ -1,9 +1,11 @@
 package DAGH_Following;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
- * Created on 2017-02-03.
+ * Created by 止水清潇 on 2017-02-03.
  */
 public class Functions {
     /*二分查找*/
@@ -21,10 +23,26 @@ public class Functions {
         }
         return 0;
     }
-    public void QuickSort(){
-
-    }
-    public int Pivot(){
-        return 0;
+    /*遍历结果集，构建字典树*/
+    public void buildDictTree(ResultSet ret,DictTreeNode treeHeadNode,FollowersNode aTargetFollowing) throws SQLException {
+        DictTreeNode currentNode = treeHeadNode;
+        while(ret.next()){
+            String commonUser = Integer.toString(ret.getInt(2));
+            for(int i = 0, loopLength = commonUser.length();i<loopLength;i++){
+                int position = commonUser.charAt(i) - '0';
+                if(currentNode.getFlag(position) == true && i == loopLength){
+                    NodeUserInfo temp = currentNode.getNodeUser();
+                    if(!temp.isExist())
+                        temp.setUserCodeAndName(Integer.parseInt(commonUser));
+                    temp.selfUpCounter();
+                    temp.addSameFollowing(aTargetFollowing);
+                }else if(currentNode.getFlag(position) && i < loopLength){
+                    currentNode = currentNode.getNextDictTreeNode(position);
+                }else{
+                    currentNode.createLinkNode(position);
+                    currentNode = currentNode.getNextDictTreeNode(position);
+                }
+            }
+        }
     }
 }
