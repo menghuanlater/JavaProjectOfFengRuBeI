1cd5bd386a85e869f0498707c28f563b38ca08e4
止水清潇 <anlin781205936@126.com>
2017-02-24 19:56:02 +0800

    Create DBConnect.java

---
+++ ProjectsLocalTest/MysqlAssist/DBConnect.java
@@ -0,0 +1,51 @@
+package MysqlAssist;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Created by 止水清潇 on 2017-01-26.
+ * 连接数据库的类
+ */
+public class DBConnect
+{
+    private static final String url = "jdbc:mysql://localhost:3306/github?characterEncoding=utf-8&useSSL=true";
+    private static final String name = "com.mysql.jdbc.Driver";
+    private static final String user = "root";
+    private static final String password = "zhishui314625ASD";
+
+    public Connection conn = null;
+    public PreparedStatement pst = null;
+
+    public DBConnect()
+    {
+        try {
+            Class.forName(name);//指定连接类型
+            conn = DriverManager.getConnection(url,user,password);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void setPst(String sql)
+    {
+        try {
+            pst = conn.prepareStatement(sql);
+            //pst.setFetchSize(Integer.MIN_VALUE);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void close()
+    {
+        try{
+            this.conn.close();
+            this.pst.close();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
